5777e6f53dd0f55e5e975c96068ff3167f7346ba
fc <fc323897@gmail.com>
2024-03-09 11:56:09 -0700

    Se modifica server.js y se agrega config.js, se instalan varias dep dev

--- server.js
+++ server.js
@@ -1,9 +1,12 @@
-import express from 'express';
+import express from "express";
+import { connect } from "./config.js";
+
+connect();
 
 const api = express();
 
 api.listen(8001, () => {
-	console.log("Escuchando puerto 8001");
+  console.log("Escuchando puerto 8001");
 });
 
 
